75392d22524c7aa4451ec63111de4dee9cd502df
Clement Enjolras <eclement@lavabit.com>
2012-03-18 20:45:25 +0100

    terminer difference multiens

--- partie1.ml
+++ partie1.ml
@@ -727,25 +727,47 @@ intersectionmultiens (Add((1,3),Add((2,4),VIDE))) (Add((3,3),Add((2,2),VIDE)));;
 profil: differencemultiens: ’e multiens -> ’e multiens -> ’e multiens 
 semantique: calcule la différence de deux multi-ensembles.
 exemples:
-  (1) 
+  (1) differencemultiens (Add((1,4),Add((2,3),VIDE))) (Add((1,1),Add((2,3),VIDE))) = Add ((1, 3), VIDE)
 Realisation :
 Equations récursives :
+differencemultiens VIDE ens2 = VIDE
+differencemultiens (Add((e1,occ1), subseq)) (ens2) = differencemultiens (l'enssemble 1 - (e1,occurence de e1 dans e2)) (ens2 sans e1), si e1 appartien a ens2
+                                                     Add ((e1,occ1), differencemultiens (subseq) (ens2) sinon
 Terminaison :
 implantation : *)
-(* /!\ Cette fonction n'est pas correct /!\ *)
 
 let rec differencemultiens (ens1:'e multiens) (ens2:'e multiens):'e multiens =
   match ens1 with
     | VIDE -> VIDE
     | Add((e1,occ1), subseq) when appartientmultiens (e1) (ens2) ->
-        let occ2 = occurencesmultiens (e1) (ens2) in differencemultiens( supprimemultiens (e1,occ2) (ens1) ) (ens2) 
+        let occ2 = occurencesmultiens (e1) (ens2) in differencemultiens( supprimemultiens (e1,occ2) (ens1) ) (supprimemultiens(e1,0)(ens2)) 
     | Add(elem,subseq) -> Add(elem, differencemultiens (subseq) (ens2));;
+
 
 (*--------------
      Tests
 ---------------*)
-differencemultiens (Add((1,1), Add((2,4)
+differencemultiens (Add((3,4),Add((2,5),VIDE))) (Add((1,1),Add((2,4),VIDE)));; 
+(* int multiens = Add ((3, 4), Add ((2, 1), VIDE))*)
 
+differencemultiens (Add((1,4),Add((2,3),VIDE))) (Add((1,1),Add((2,3),VIDE)));;
+(* int multiens =  Add ((1, 3), VIDE) *)
+
+
+
+
+
+let rec differencemultiens (ens1: 'e multiens) (ens2: 'e multiens) : 'e multiens =
+if egauxmultiens (ens1) (ens2) then VIDE
+else if inclusmultiens (ens2) (ens1) then match ens2 with 
+    |Add((a,b),Add(d,g)) -> if appartientmultiens (a) (ens1) then
+differencemultiens (supprimemultiens ((a,b)) (ens1)) (Add(d,g)) else differencemultiens
+(ens1) (Add(d,g))
+    |Add((a,b),VIDE) -> if appartientmultiens (a) (ens1) then
+supprimemultiens ((a,b)) (ens1) else ens1
+    |VIDE-> ens1 
+else if inclusmultiens (ens1) (ens2) then VIDE 
+else ens1;;
 (*
 profil: differencesymetriquemultiens: ’e multiens -> ’e multiens -> ’e multiens
 semantique:  calcule la différence symétrique de deux multi-ensembles.
@@ -758,3 +780,5 @@ let differencesymetriquemultiens (ens1: 'e multiens) (ens2: 'e multiens) : 'e mu
 (*--------------
      Tests
 ---------------*)
+
+
